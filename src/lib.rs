6a19903cc6c7dd72048cae132abde3c62860ab32
Daniel Stahl <danstahl1138@gmail.com>
2018-07-14 14:09:20 -0500

    attempted mutable references

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
 extern crate rand;
+extern crate byteorder;
+use byteorder::{LittleEndian, WriteBytesExt};
 
-use rand::prelude::*;
+
 use rand::{thread_rng, ThreadRng, SeedableRng, Rng, StdRng};
 use rand::distributions::Uniform;
-use rand::distributions::Normal;
+use rand::distributions::StandardNormal;
+use rand::distributions::{Distribution};
+
 #[macro_use]
 #[cfg(test)]
 extern crate approx;
 
-struct Upper_lower {
+pub struct Upper_lower {
     lower: f64,
     upper: f64,
 }
@@ -44,23 +48,30 @@ fn get_truncated_parameter(
     if result>upper {upper} else if result<lower {lower} else {result}
 }
 
-fn get_random_parameters<T>(
+fn get_random_parameters<T, U>(
     ul:&Vec<Upper_lower>,
-    rand_generator:T
+    rng:&mut T,
+    rand:&mut U
 )->Vec<f64>
-    where T:Fn()->f64
+    where 
+        T:Rng,
+        U:Distribution<f64>
 {
-    ul.iter().map(|v|get_random_parameter(v.lower, v.upper, rand_generator())).collect()
+    ul.iter().map(|v|get_random_parameter(v.lower, v.upper, rand.sample(rng))).collect()
 }
 
-fn get_new_parameter_and_fn<T>(
+fn get_new_parameter_and_fn<T, U, S>(
     ul:&Vec<Upper_lower>,
-    obj_fn:&impl Fn(&Vec<f64>)->f64,
-    rand_generator:T
+    obj_fn:S,
+    rng:&mut T,
+    rand:&mut U
 )->(Vec<f64>, f64) 
-    where T: Fn()->f64
+    where 
+        S:Fn(&Vec<f64>)->f64,
+        T:Rng,
+        U:Distribution<f64>
 {
-    let parameters=get_random_parameters(ul, &rand_generator);
+    let parameters=get_random_parameters(ul, rng, rand);
     let fn_value_at_parameters=obj_fn(&parameters);
     (parameters, fn_value_at_parameters)
 }
@@ -70,53 +81,64 @@ fn get_step_size(curr:f64, best:f64, lower:f64, upper:f64)->f64{
     step_increment*(upper-lower)*(curr-best)
 }
 
-fn get_new_nest<T, S>(
+fn get_new_nest<T, U, S>(
     ul:&Vec<Upper_lower>, 
     obj_fn:S,
-    rand_generator:T,
-    n:usize
+    n:usize,
+    rng:&mut T,
+    rand:&mut U
 )->Vec<(Vec<f64>, f64)>
-    where S: Fn(&Vec<f64>)->f64,
-    T: Fn()->f64
+    where 
+        S: Fn(&Vec<f64>)->f64,
+        T:Rng,
+        U:Distribution<f64>
 {
-    (0..n).map(|_|get_new_parameter_and_fn(ul, &obj_fn, &rand_generator)).collect()
+    (0..n).map(|_|get_new_parameter_and_fn(ul, &obj_fn, rng, rand)).collect()
 }
 
 fn sort_nest(
-    nest:Vec<(Vec<f64>, f64)>//move nest
-)->Vec<(Vec<f64>, f64)>{
-    nest.sort_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap());//smallest to largest...I hope the compiler optimizes this
-    nest
+    nest:&mut Vec<(Vec<f64>, f64)>//move nest
+){
+    nest.sort_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap());//smallest to largest
 }
 
 fn get_best_nest(
     new_nest:&Vec<(Vec<f64>, f64)>,
-    curr_nest:Vec<(Vec<f64>, f64)>//move curr_nest
-)->Vec<(Vec<f64>, f64)>
-{
-    sort_nest(curr_nest.into_iter().zip(new_nest.iter()).map(|(curr_val, new_val)|{
+    curr_nest:&mut Vec<(Vec<f64>, f64)>//move curr_nest
+){
+    curr_nest.iter_mut().zip(new_nest.iter()).for_each(|(mut curr_val, new_val)|{
         let (curr_params, curr_fn_val)=curr_val;
         let (new_params, new_fn_val)=new_val;
-        *if new_fn_val< &curr_fn_val { &new_val } else { &curr_val }
-    }).collect::<Vec<_>>())
+        if new_fn_val< curr_fn_val { 
+            curr_params=new_params.iter().map(|v|*v).collect();
+            curr_fn_val=new_fn_val;
+        } 
+    });
+    sort_nest(curr_nest);
 }
 
-fn get_cuckoos<T, S>(
-    new_nest:Vec<(Vec<f64>, f64)>, //move for efficiency (return self)
+fn get_cuckoos<T, G, U>(
+    new_nest:&mut Vec<(Vec<f64>, f64)>, 
     curr_nest:&Vec<(Vec<f64>, f64)>,
     best_parameters:&Vec<f64>,
     ul:&Vec<Upper_lower>,
     obj_fn:impl Fn(&Vec<f64>)->f64,
     lambda:f64,
-    uniform_rand_generator:T,
-    normal_rand_generator:S
-)->Vec<(Vec<f64>, f64)>
-    where T:Fn()->f64, S:Fn()->f64
+    rng:&mut T,
+    uniform:&mut U,
+    normal:&mut G
+)
+    where
+        T:Rng,
+        G:Distribution<f64>,
+        U:Distribution<f64>
 {
-    new_nest.into_iter()
+    new_nest.iter_mut()
         .zip(curr_nest.iter())
-        .map(|((_, _), (curr_parameters, _))|{
-            let new_nest_parameters=curr_parameters.iter()
+        .for_each(|(&mut new_val, curr_val)|{
+            let (new_parameters, new_fn_val)=new_val;
+            let (curr_parameters,_)=curr_val;
+            new_parameters=curr_parameters.iter()
                 .zip(ul.iter())
                 .zip(best_parameters.iter())
                 .map(|((curr_param, v), bp)|{
@@ -126,15 +148,15 @@ fn get_cuckoos<T, S>(
                             *curr_param, 
                             get_step_size(*curr_param, *bp, v.lower, v.upper), 
                             lambda, 
-                            uniform_rand_generator(), 
-                            normal_rand_generator()
+                            uniform.sample(rng), 
+                            normal.sample(rng)
                         )
                     )
                 }).collect();
             
-            let new_nest_fn=obj_fn(&new_nest_parameters);
-            (new_nest_parameters, new_nest_fn)
-        }).collect()
+            new_fn_val=obj_fn(&new_parameters);
+            //(new_nest_parameters, new_nest_fn)
+        });//.collect()
 }
 
 fn get_pa(
@@ -146,31 +168,43 @@ fn get_pa(
     p_max-(p_max-p_min)*(index as f64)/(n as f64)
 }
 
-fn empty_nests<'a, T>(
-    new_nest:Vec<(Vec<f64>, f64)>, //move this for efficiency (can return self)
+fn empty_nests<T, U>(
+    new_nest:&mut Vec<(Vec<f64>, f64)>, //
     obj_fn:&impl Fn(&Vec<f64>)->f64,
-    rand_generator:T,
     ul:&Vec<Upper_lower>,
-    p:f64
-)->Vec<(Vec<f64>, f64)>
-    where T: Fn()->f64,
+    p:f64,
+    rng:&mut T,
+    rand:&mut U
+)
+    where 
+        T:Rng,
+        U:Distribution<f64>
 {
     let n=new_nest.len();
     let num_to_keep=((n as f64)*p) as usize;
     let start_num=n-num_to_keep;
-    new_nest.into_iter().enumerate().map(|(index, v)|{
-        if index<start_num {v} else {get_new_parameter_and_fn(ul, &obj_fn, &rand_generator)}
-    }).collect()
+    new_nest.iter_mut().enumerate().for_each(|(index, &mut new_val)|{
+        //let (new_parameters, new_fn_val)=new_val;
+        if index>=start_num {
+            new_val=get_new_parameter_and_fn(ul, &obj_fn, rng, rand);
+        }
+    });
 }
 
-fn get_rng_seed(seed:&[u8;32])->StdRng{
-    let mut rng: StdRng = SeedableRng::from_seed(*seed); 
-    rng
+pub fn get_rng_seed(seed:i32)->StdRng{
+    let mut wtr = vec![];
+    wtr.write_i32::<LittleEndian>(seed).unwrap();
+
+    let mut array = [0; 32];
+    let bytes = &wtr[..array.len()]; // panics if not enough data
+    array.copy_from_slice(bytes);
+
+
+    SeedableRng::from_seed(array) 
 }
 
-fn get_rng_system_seed()->ThreadRng{
-    let mut rng: ThreadRng=thread_rng();
-    rng
+pub fn get_rng_system_seed()->ThreadRng{
+    thread_rng()
 }
 
 pub fn optimize<T>(
@@ -181,43 +215,42 @@ pub fn optimize<T>(
     tol:f64,
     rng_inst:impl Fn()->T
 )->(Vec<f64>, f64)
-    where T:RngCore+CryptoRng
+    where T:Rng
 {
     
-    let mu=0.0;
-    let sigma=1.0;
     let lambda=1.5;//controls size of levy moves
     let p_min=0.05;//min percentage of nests to replace
     let p_max=0.5;//max percentage of nests to replace
 
     //randomness
     let mut rng=rng_inst();
-    let normal = Normal::new(mu, sigma);
-    let uniform = Uniform::new(0.0f64, 1.0);
-    let normal_rand_generator=||normal.sample(&mut rng);
-    let uniform_rand_generator=||uniform.sample(&mut rng);
+    let mut normal=StandardNormal;
+    let mut uniform=Uniform::new(0.0f64, 1.0);
+    let mut curr_nest=get_new_nest(&ul, &obj_fn, n, &mut rng, &mut normal);
+    sort_nest(&mut curr_nest);
 
-    let mut curr_nest=sort_nest(get_new_nest(&ul, &obj_fn, &normal_rand_generator, n));
-
-    let mut new_nest=get_new_nest(&ul, &obj_fn, &normal_rand_generator, n);
+    let mut new_nest=get_new_nest(&ul, &obj_fn, n, &mut rng, &mut normal);
 
     let mut done = false; // mut done: bool
     let mut index=0;
     while !done {
         let (curr_best_params, _)=curr_nest.first().unwrap();
-        new_nest=get_cuckoos(
-            new_nest, &curr_nest, 
+        get_cuckoos(
+            &mut new_nest, &curr_nest, 
             &curr_best_params, &ul, 
             &obj_fn, lambda, 
-            &uniform_rand_generator, &normal_rand_generator
+            &mut rng, &mut uniform, &mut normal
         );
-        curr_nest=sort_nest(
-            empty_nests(
-                get_best_nest(&new_nest, curr_nest), 
-                &obj_fn, &normal_rand_generator, &ul, 
-                get_pa(p_min, p_max, index, total_mc)
-            )
+        get_best_nest(&new_nest, &mut curr_nest);
+        empty_nests(
+            &mut new_nest, 
+            &obj_fn, &ul,
+            get_pa(p_min, p_max, index, total_mc),
+            &mut rng,
+            &mut normal
         );
+        sort_nest(&mut new_nest);
+
         let (_, fn_min)=curr_nest.first().unwrap();
         index=index+1;
         done=index>=total_mc || fn_min<=&tol; 
